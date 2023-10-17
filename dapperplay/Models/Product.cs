94431dabd7a598f7fde5d3e2dc9f925c6e923bf0
Cam Soper <Cam.Soper@microsoft.com>
2023-10-17 20:02:35 +0000

    wip

--- dapperplay/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace dapperplay.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
